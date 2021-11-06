f3af84610fca3e7631ea7e2fe005133114903909
Víctor Santiago <victorsantiago.matesub@gmail.com>
2021-11-06 01:06:52 +0100

    First implementation of the Crank-Nicolson method with Conjugated Gradient old method

--- auxFunctions.h
+++ auxFunctions.h
@@ -1,1 +1,60 @@
 #include "functions.h"
+
+
+double **store_matrix(int rows, int cols) {
+    double **A;
+    int i;
+    A = (double**)calloc(rows, sizeof(double*));
+    if(A == NULL) {
+        printf("Error storing matrix.\n");
+        exit(1);
+    }
+    for(i = 0; i < rows; i++) {
+        A[i] = (double*)calloc(cols, sizeof(double));
+        if(A[i] == NULL) {
+            printf("Error storing matrix.\n");
+            exit(1);
+        }
+    }
+    return A;
+}
+
+double free_matrix(double **A, int cols) {
+    int i;
+    for(i = 0; i < cols; i++) free(A[i]);
+    free(A);
+}
+
+double special_product(double **x, double **mat, int dim) {
+    int i, j;
+    for(i = 0; i < dim; i++)
+        for(j = 0; j < dim; j++)
+            x[i][j] = 4*mat[i][j]-mat[i-1][j]-mat[i+1][j]-mat[i][j-1]-mat[i][j+1];
+}
+
+double sup_norm(double **x, double dim) {
+    int i, j;
+    double norm = 0.;
+    for(i = 0; i < dim; i++)
+        for(j = 0; j < dim; j++)
+            if(fabs(x[i][j]) < norm)
+                norm = fabs(x[i][j]);
+    return norm;
+}
+
+double matrix_sum(double **x, double **y, int dim) {
+    int i, j;
+    for(i = 0; i < dim; i++)
+        for(j = 0; j < dim; j++)
+            x[i][j] += y[i][j];
+}
+
+double vector_product_matrix(double **x, double **y, int dim) {
+    int i, j;
+    double prod = 0.;
+    for(i = 0; i < dim; i++)
+        for(j = 0; j < dim; j++)
+            prod += x[i][j]*y[i][j];
+    return prod;
+}
+
--- methods.h
+++ methods.h
@@ -1,3 +1,71 @@
 #include "auxFunctions.h"
 
 #define T 2*PI
+
+#include "auxFunctions.h"
+
+double boundary_values(double **A, double t, double h, int dim, int option) {
+    int i;
+    for(i = 0; i < dim; i++) {
+        A[0][i] = g(t, 0, h*i, option);
+        A[i][0] = g(t, h*i, 0, option);
+        A[dim-1][i] = g(t, h*(dim+1), h*i, option);
+        A[i][dim-1] = g(t, h*i, h*(dim+1), option);
+    }
+}
+
+double crank_nicloson(double **u, double h, double t, double dt, int dim, double tol, int option) {
+    int i, j;
+    double alpha = dt/(2*h*h);
+    for(i = 0; i < dim; i++)
+        for(j = 0; j < dim; j++)
+            u[i][j] += 2*(1-2*alpha)*u[i][j] + alpha*(u[i+1][j]+u[i-1][j]+u[i][j+1]+u[i][j-1]);
+}
+
+double conjugated_gradient(double **A, double tol, int dim, double h, double t, int option) {
+    int i, j, it_num = 0;
+    double **p, **q, **r;
+    double alpha, beta, pre_norm, post_norm;
+
+    p = store_matrix(dim+2, dim+2);
+    q = store_matrix(dim+2, dim+2);
+    r = store_matrix(dim+2, dim+2);
+
+    // Store A·r in r
+    special_product(r, A, dim+2);
+    // Iteration number 0:
+    for(i = 0; i < dim+1; i++) {
+        for(j = 0; j < dim+1; j++) {
+            r[i][j] = h*h*f(t, h*i, h*j, option) - r[i][j];
+            p[i][j] = r[i][j];
+        }
+    }
+    // Iterations of CG method
+    do {
+        // Now q = A·p and p remains p
+        special_product(q, p, dim+2);
+        // I define this to save time on implementing the function
+        pre_norm = vector_product_matrix(r, r, dim+2);
+        // p^T·A·p in denominator
+        alpha = (-1.*pre_norm)/vector_product_matrix(p, q, dim+2);
+        // Update values of A and r in the iteration k+1
+        for(i = 1; i < dim+1; i++) {
+            for(j = 1; j < dim+1; j++) {
+                A[i][j] -= alpha*p[i][j];
+                r[i][j] += alpha*q[i][j];
+            }
+        }
+        // Now we calculate norm_post, after the update, to calculate beta
+        post_norm = vector_product_matrix(r, r, dim+2);
+        // Now we calculate beta
+        beta = post_norm/pre_norm;
+        // With beta already calculated, we update the value of p in the iteration k+1
+        for(i = 0; i < dim+1; i++)
+            for(j = 0; j < dim+1; j++)
+                p[i][j] = r[i][j] + beta;
+        // Acumulate the number of iterations
+        it_num++;
+    // Finishing condition of the method itself
+    } while(post_norm >= tol*tol);
+    return it_num;
+}
